cb6152bd7cbe6e81fbadf5061d50d4f92e05de9c
Jan LACINA <jan.lacina@cz.abb.com>
2023-07-20 14:38:57 +0200

    DBContext base with nuget packages

---
+++ CRUD-DB/CRUD-DB/Database/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CRUD_DB.Database
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) {}
+
+        //public virtual DbSet<Car> Cars { get; set; }
+    }
+}
